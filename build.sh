ccc2d37cc26f6563ef12262711603a169c166dc7
mrf7777 <micryfulg@gmail.com>
2024-07-29 18:33:11 -0500

    make setup script disable audio; build script includes http library headers

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 g++ \
 	-std=c++17\
        	-I ~/rpi-rgb-led-matrix/include/\
+	-I ./cpp-httplib\
        	-L ~/rpi-rgb-led-matrix/lib\
        	./main.cpp\
        	-l:librgbmatrix.a\
--- setup.sh
+++ setup.sh
@@ -1,7 +1,20 @@
 sudo apt update
 sudo apt upgrade
 
+# required for some utilites in the rpi rgb led matrix library
 sudo apt-get install libgraphicsmagick++-dev libwebp-dev -y
+
+# disable the sound module if not disabled already
+blacklist_file=/etc/modprobe.d/alsa-blacklist.conf
+sudo touch $blacklist_file
+if sudo grep -q "audio_has_been_blacklisted_evidence" $blacklist_file; then
+	echo "Audio already disabled."
+else
+	echo "" | sudo tee -a $blacklist_file
+	echo "# auto-generated audio blacklist from protogen software project setup script" | sudo tee -a $blacklist_file
+	echo "# audio_has_been_blacklisted_evidence" | sudo tee -a $blacklist_file
+	echo "blacklist snd_bcm2835" | sudo tee -a $blacklist_file
+fi
 
 cd ~/rpi-rgb-led-matrix
 make
