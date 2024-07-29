c258bd79443cbaec2d705f7b8193b619cece38b8
mrf7777 <micryfulg@gmail.com>
2024-07-29 18:47:16 -0500

    add test http server in main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <led-matrix.h>
 #include <pixel-mapper.h>
 
+#include <httplib.h>
+
 int main() {
 	std::cout << "Hello, World!" << std::endl;
 
@@ -15,4 +17,12 @@ int main() {
 		options,
 		runtime_options
 	));
+
+	httplib::Server srv;
+
+	srv.Get("/", [](const httplib::Request & req, httplib::Response & res){
+			res.set_content("Hello, World!", "text/plain");
+	});
+
+	srv.listen("0.0.0.0", 8080);
 }
