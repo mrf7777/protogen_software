38356470d19e78c56c3efb0079d52fd57669b4cc
mrf7777 <micryfulg@gmail.com>
2024-08-02 17:56:30 -0500

    keep matrix updating on the main thread

--- main.cpp
+++ main.cpp
@@ -30,43 +30,51 @@ void data_viewer_thread_function(std::shared_ptr<AppState> app_state, std::uniqu
 	}
 }
 
+void web_server_thread_function(std::unique_ptr<httplib::Server> server) {
+	server->listen("0.0.0.0", 8080);
+}
+
 int main(int argc, char *argv[]) {
 	Magick::InitializeMagick(*argv);
 	
 	auto app_state = std::shared_ptr<AppState>(new AppState());
 
-	httplib::Server srv;
+	auto srv = std::unique_ptr<httplib::Server>(new httplib::Server());
 
-	srv.set_logger([=](const auto& req, const auto& res){
+	srv->set_logger([=](const auto& req, const auto& res){
 	});
 
-	srv.Get("/", [](const httplib::Request & req, httplib::Response & res){
+	srv->Get("/", [](const httplib::Request & req, httplib::Response & res){
 			res.set_content(read_file_to_str("./index.html"), "text/html");
 	});
 
-	srv.Put("/protogen/head/emotion", [app_state](const auto& req, auto& res){
+	srv->Put("/protogen/head/emotion", [app_state](const auto& req, auto& res){
 			const auto emotion = ProtogenHeadState::emotionFromString(req.body);
 			app_state->protogenHeadState().setEmotion(emotion);
 	});	
-	srv.Put("/protogen/head/mouth/color", [app_state](const auto& req, auto& res){
+	srv->Put("/protogen/head/mouth/color", [app_state](const auto& req, auto& res){
 			app_state->protogenHeadState().setMouthColor(req.body);
 	});
-	srv.Put("/protogen/head/eye/color", [app_state](const auto& req, auto& res){
+	srv->Put("/protogen/head/eye/color", [app_state](const auto& req, auto& res){
 			app_state->protogenHeadState().setEyeColor(req.body);
 	});
 
 	// use phone as a microphone/audio provider
-	auto web_audio_provider = std::unique_ptr<audio::WebsiteAudioProvider>(new audio::WebsiteAudioProvider(srv, "/protogen/head/audio-loudness"));
+	auto web_audio_provider = std::unique_ptr<audio::WebsiteAudioProvider>(new audio::WebsiteAudioProvider(*srv, "/protogen/head/audio-loudness"));
 
 	auto data_viewer = std::unique_ptr<IViewData<AppState>>(new ProtogenHeadMatrices(argc, argv, std::move(web_audio_provider)));
 
-	auto ret = srv.set_mount_point("/static", "./static");
+	auto ret = srv->set_mount_point("/static", "./static");
 	if(!ret) {
 		std::cerr << "Could not mount static directory to web server." << std::endl;
 	}
 
 	// start threads
-	std::thread data_viewer_thread(data_viewer_thread_function, app_state, std::move(data_viewer));
-
-	srv.listen("0.0.0.0", 8080);
+	std::thread web_server_thread(web_server_thread_function, std::move(srv));
+	
+	static const int FPS = 30;
+	while(true) {
+		data_viewer->viewData(*app_state);
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000/FPS));
+	}
 }
