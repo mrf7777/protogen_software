b9317f3394b4fab5a079b118790f5a8a52ebc862
mrf7777 <micryfulg@gmail.com>
2024-10-06 16:57:49 -0500

    add player now works

--- static/minecraft.js
+++ static/minecraft.js
@@ -1,49 +1,37 @@
-const origin = window.location.origin
-
-function startMinecraft() {
-    fetch(`${origin}/protogen/mode`, {method: "put", body: "minecraft"})
-}
-
-function getPlayers(callback) {
-    fetch(`${origin}/protogen/minecraft/players`)
-        .then(response => response.text)
-        .then(text => {
-            const players = text.split(/\r?\n/).filter(player => player !== "")
-            callback(players)
-        })
-}
-
-function createPlayer(player_id) {
-    fetch(`${origin}/protogen/minecraft/players/${player_id}`, {method: "put"})
-}
-
-function deletePlayer(player_id) {
-    fetch(`${origin}/protogen/minecraft/players/${player_id}`, {method: "delete"})
-}
-
-function playerMove(player_id, move_direction) {
-    fetch(`${origin}/protogen/minecraft/players/${player_id}/move`, {method: "post", body: move_direction})
-}
-
-function playerPlaceBlock(player_id) {
-    fetch(`${origin}/protogen/minecraft/players/${player_id}/place_block`, {method: "post"})
-}
-
-function setPlayerBlock(player_id, block) {
-    fetch(`${origin}/protogen/minecraft/players/${player_id}/block`, {method: "put", body: block})
-}
-
-function generateWorld(seed) {
-    fetch(`${origin}/protogen/minecraft/world/generate`, {method: "put", body: seed})
-}
-
 let enable_button = document.getElementById("enable-button")
 enable_button.addEventListener("click", () => {
 	startMinecraft()
 })
 
-
 let seedInput = document.getElementById("seed-input")
 document.getElementById("generate-button").addEventListener("click", () => {
     generateWorld(seedInput.value)
+})
+
+let player_list = document.getElementById("players-list")
+function createPlayerElement(player_id) {
+	let player_list_item = document.createElement("li")
+	let player_user_interface_link_element = document.createElement("a")
+	player_user_interface_link_element.href = `${origin}/protogen/minecraft/interface?player_id=${player_id}`
+	player_user_interface_link_element.appendChild(document.createTextNode("Play as"))
+	let player_id_element = document.createTextNode(player_id)
+	player_list_item.appendChild(player_user_interface_link_element)
+	player_list_item.appendChild(player_id_element)
+	return player_list_item
+}
+function populatePlayerList() {
+	getPlayers((players) => {
+		player_list.innerHTML = ""
+		players.forEach((player) => {
+			player_list.appendChild(createPlayerElement(player))
+		})
+	})
+}
+populatePlayerList()
+setInterval(populatePlayerList, 2000)
+
+let add_player_button = document.getElementById("add-player-button")
+add_player_button.addEventListener("click", () => {
+	let player_id = window.prompt("Enter new player name.")
+	createPlayer(player_id)
 })
---
+++ static/minecraft_endpoints.js
@@ -0,0 +1,38 @@
+const origin = window.location.origin
+
+function startMinecraft() {
+    fetch(`${origin}/protogen/mode`, {method: "put", body: "minecraft"})
+}
+
+function getPlayers(callback) {
+    fetch(`${origin}/protogen/minecraft/players`)
+        .then(response => response.text())
+        .then(text => {
+            const players = text.split(/\r?\n/).filter(player => player !== "")
+            callback(players)
+        })
+}
+
+function createPlayer(player_id) {
+    fetch(`${origin}/protogen/minecraft/players/${player_id}`, {method: "put"})
+}
+
+function deletePlayer(player_id) {
+    fetch(`${origin}/protogen/minecraft/players/${player_id}`, {method: "delete"})
+}
+
+function playerMove(player_id, move_direction) {
+    fetch(`${origin}/protogen/minecraft/players/${player_id}/move`, {method: "post", body: move_direction})
+}
+
+function playerPlaceBlock(player_id) {
+    fetch(`${origin}/protogen/minecraft/players/${player_id}/place_block`, {method: "post"})
+}
+
+function setPlayerBlock(player_id, block) {
+    fetch(`${origin}/protogen/minecraft/players/${player_id}/block`, {method: "put", body: block})
+}
+
+function generateWorld(seed) {
+    fetch(`${origin}/protogen/minecraft/world/generate`, {method: "put", body: seed})
+}
