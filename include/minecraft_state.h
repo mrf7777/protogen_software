f83f9f7d01a5f7e77a0c20a4cbdec8a980e39a01
Michael Fulghum <micryfulg@gmail.com>
2024-10-19 22:47:40 -0500

    extract minecraft state into h and cpp files

--- include/minecraft_state.h
+++ include/minecraft_state.h
@@ -10,67 +10,22 @@
 
 class MinecraftPlayerState final {
 public:
+
+    using CursorPos = std::pair<std::size_t, std::size_t>;
+
 	enum class CursorDirection {
 		Up, Down, Left, Right,
 	};
-	static CursorDirection stringToCursorDirection(const std::string& cd) {
-		if(cd == "up")
-			return CursorDirection::Up;
-		else if(cd == "down")
-			return CursorDirection::Down;
-		else if(cd == "left")
-			return CursorDirection::Left;
-		else if(cd == "right")
-			return CursorDirection::Right;
-		else
-			return CursorDirection::Up;
-	}
-	using CursorPos = std::pair<std::size_t, std::size_t>;
-	MinecraftPlayerState(std::size_t start_row, std::size_t start_col, std::size_t max_row, std::size_t max_col)
-		: m_cursor{start_row, start_col},
-		m_maxRow{max_row},
-		m_maxCol{max_col},
-		m_selectedBlock(mc::Block(mc::DirtBlock()))
-	{
-		correctCursor();
-	}
-	mc::Block selectedBlock() const {
-		return m_selectedBlock;
-	}
-	void setSelectedBlock(const mc::Block& b) {
-		m_selectedBlock = b;
-	}
-	CursorPos cursor() const {
-		return m_cursor;
-	}
-	void moveCursor(CursorDirection direction) {
-		switch(direction) {
-		case CursorDirection::Up:
-			if(m_cursor.first > 0)
-				m_cursor.first -= 1;
-			break;
-		case CursorDirection::Down:
-			if(m_cursor.first < (m_maxRow - 1))
-				m_cursor.first += 1;
-			break;
-		case CursorDirection::Left:
-			if(m_cursor.second > 0)
-				m_cursor.second -= 1;
-			break;
-		case CursorDirection::Right:
-			if(m_cursor.second < (m_maxCol - 1))
-				m_cursor.second += 1;
-			break;
-		}
-	}
+	static CursorDirection stringToCursorDirection(const std::string& cd);
+	
+	MinecraftPlayerState(std::size_t start_row, std::size_t start_col, std::size_t max_row, std::size_t max_col);
+
+	mc::Block selectedBlock() const; 
+	void setSelectedBlock(const mc::Block& b);
+	CursorPos cursor() const;
+	void moveCursor(CursorDirection direction);
 private:
-	void correctCursor() {
-		// If cursor is out of bounds, put it back in bounds.
-		if(m_cursor.first >= m_maxRow)
-			m_cursor.first = m_maxRow - 1;
-		if(m_cursor.second >= m_maxCol)
-			m_cursor.second = m_maxCol - 1;
-	}
+	void correctCursor();
 
 	CursorPos m_cursor;
 	std::size_t m_maxRow;
@@ -82,69 +37,18 @@ class MinecraftState final {
 public:
 	using PlayerId = std::string;
 
-	MinecraftState()
-		: m_blockMatrix(32, 128), 
-		m_players(),
-		m_blockColorProfile(mc::defaultBlockColorProfile)
-	{}
-	const mc::BlockMatrix& blockMatrix() const { return m_blockMatrix; }
-	mc::BlockMatrix& blockMatrix() { return m_blockMatrix; }
-	const mc::BlockColorProfile blockColorProfile() const { return m_blockColorProfile; }
-	bool addNewPlayer(const PlayerId& id) {
-		std::lock_guard<std::mutex> lock(m_playerMutex);
-		if(m_players.contains(id)) {
-			return false;
-		} else {
-			const std::size_t number_players = m_players.size();
-			const auto result = m_players.insert(std::make_pair(
-				id,
-				MinecraftPlayerState(0, number_players, 32, 128)
-			));
-			return true;
-		}
-	}
-	void removePlayer(const PlayerId& id) {
-		std::lock_guard<std::mutex> lock(m_playerMutex);
-		m_players.erase(id);
-	}
-	bool accessPlayer(const PlayerId& id, std::function<void(MinecraftPlayerState&)> accessor) {
-		std::lock_guard<std::mutex> lock(m_playerMutex);
-		if(m_players.contains(id)) {
-			accessor(m_players.at(id));
-			return true;
-		} else {
-			return false;
-		}
-	}
-	bool accessPlayer(const PlayerId& id, std::function<void(const MinecraftPlayerState&)> accessor) const {
-		std::lock_guard<std::mutex> lock(m_playerMutex);
-		if(m_players.contains(id)) {
-			accessor(m_players.at(id));
-			return true;
-		} else {
-			return false;
-		}
-	}
-	std::vector<PlayerId> players() const {
-		std::lock_guard<std::mutex> lock(m_playerMutex);
-		return _players();
-	}
-	std::string playersSeperatedByNewline() const {
-		std::lock_guard<std::mutex> lock(m_playerMutex);
-		std::string players_string;
-		for(const auto player : _players()) {
-			players_string += player + "\n";
-		}
-		return players_string;
-	}
+	MinecraftState();
+	const mc::BlockMatrix& blockMatrix() const;
+	mc::BlockMatrix& blockMatrix();
+	const mc::BlockColorProfile blockColorProfile() const;
+	bool addNewPlayer(const PlayerId& id);
+	void removePlayer(const PlayerId& id);
+	bool accessPlayer(const PlayerId& id, std::function<void(MinecraftPlayerState&)> accessor);
+	bool accessPlayer(const PlayerId& id, std::function<void(const MinecraftPlayerState&)> accessor) const;
+	std::vector<PlayerId> players() const;
+	std::string playersSeperatedByNewline() const;
 private:
-	std::vector<PlayerId> _players() const {
-		std::vector<PlayerId> players;
-		for(const auto& player : m_players) {
-			players.push_back(player.first);
-		}
-		return players;
-	}
+	std::vector<PlayerId> _players() const;
 
 	mc::BlockMatrix m_blockMatrix;
 	mc::BlockColorProfile m_blockColorProfile;
---
+++ src/minecraft_state.cpp
@@ -0,0 +1,137 @@
+#include <minecraft_state.h>
+
+MinecraftPlayerState::CursorDirection MinecraftPlayerState::stringToCursorDirection(const std::string& cd) {
+    if(cd == "up")
+        return CursorDirection::Up;
+    else if(cd == "down")
+        return CursorDirection::Down;
+    else if(cd == "left")
+        return CursorDirection::Left;
+    else if(cd == "right")
+        return CursorDirection::Right;
+    else
+        return CursorDirection::Up;
+}
+
+MinecraftPlayerState::MinecraftPlayerState(std::size_t start_row, std::size_t start_col, std::size_t max_row, std::size_t max_col)
+    : m_cursor{start_row, start_col},
+    m_maxRow{max_row},
+    m_maxCol{max_col},
+    m_selectedBlock(mc::Block(mc::DirtBlock()))
+{
+    correctCursor();
+}
+
+mc::Block MinecraftPlayerState::selectedBlock() const {
+    return m_selectedBlock;
+}
+
+void MinecraftPlayerState::setSelectedBlock(const mc::Block& b) {
+    m_selectedBlock = b;
+}
+
+MinecraftPlayerState::CursorPos MinecraftPlayerState::cursor() const {
+    return m_cursor;
+}
+
+void MinecraftPlayerState::moveCursor(CursorDirection direction) {
+    switch(direction) {
+    case CursorDirection::Up:
+        if(m_cursor.first > 0)
+            m_cursor.first -= 1;
+        break;
+    case CursorDirection::Down:
+        if(m_cursor.first < (m_maxRow - 1))
+            m_cursor.first += 1;
+        break;
+    case CursorDirection::Left:
+        if(m_cursor.second > 0)
+            m_cursor.second -= 1;
+        break;
+    case CursorDirection::Right:
+        if(m_cursor.second < (m_maxCol - 1))
+            m_cursor.second += 1;
+        break;
+    }
+}
+
+void MinecraftPlayerState::correctCursor() {
+    // If cursor is out of bounds, put it back in bounds.
+    if(m_cursor.first >= m_maxRow)
+        m_cursor.first = m_maxRow - 1;
+    if(m_cursor.second >= m_maxCol)
+        m_cursor.second = m_maxCol - 1;
+}
+
+MinecraftState::MinecraftState()
+    : m_blockMatrix(32, 128), 
+    m_players(),
+    m_blockColorProfile(mc::defaultBlockColorProfile)
+{}
+
+const mc::BlockMatrix& MinecraftState::blockMatrix() const { return m_blockMatrix; }
+
+mc::BlockMatrix& MinecraftState::blockMatrix() { return m_blockMatrix; }
+
+const mc::BlockColorProfile MinecraftState::blockColorProfile() const { return m_blockColorProfile; }
+
+bool MinecraftState::addNewPlayer(const PlayerId& id) {
+    std::lock_guard<std::mutex> lock(m_playerMutex);
+    if(m_players.contains(id)) {
+        return false;
+    } else {
+        const std::size_t number_players = m_players.size();
+        const auto result = m_players.insert(std::make_pair(
+            id,
+            MinecraftPlayerState(0, number_players, 32, 128)
+        ));
+        return true;
+    }
+}
+
+void MinecraftState::removePlayer(const PlayerId& id) {
+    std::lock_guard<std::mutex> lock(m_playerMutex);
+    m_players.erase(id);
+}
+
+bool MinecraftState::accessPlayer(const PlayerId& id, std::function<void(MinecraftPlayerState&)> accessor) {
+    std::lock_guard<std::mutex> lock(m_playerMutex);
+    if(m_players.contains(id)) {
+        accessor(m_players.at(id));
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool MinecraftState::accessPlayer(const PlayerId& id, std::function<void(const MinecraftPlayerState&)> accessor) const {
+    std::lock_guard<std::mutex> lock(m_playerMutex);
+    if(m_players.contains(id)) {
+        accessor(m_players.at(id));
+        return true;
+    } else {
+        return false;
+    }
+}
+
+std::vector<MinecraftState::PlayerId> MinecraftState::players() const {
+    std::lock_guard<std::mutex> lock(m_playerMutex);
+    return _players();
+}
+
+std::string MinecraftState::playersSeperatedByNewline() const {
+    std::lock_guard<std::mutex> lock(m_playerMutex);
+    std::string players_string;
+    for(const auto player : _players()) {
+        players_string += player + "\n";
+    }
+    return players_string;
+}
+
+std::vector<MinecraftState::PlayerId> MinecraftState::_players() const {
+    std::vector<PlayerId> players;
+    for(const auto& player : m_players) {
+        players.push_back(player.first);
+    }
+    return players;
+}
