7c206718a877f445c2a986bcaf8abaa5207e4ba9
Michael Fulghum <micryfulg@gmail.com>
2024-10-19 21:49:05 -0500

    split utils into h and cpp files

--- include/utils.h
+++ include/utils.h
@@ -31,44 +31,17 @@ public:
 	virtual std::string toString() const = 0;
 };
 
-static void writeImageToCanvas(const Magick::Image &img, rgb_matrix::Canvas* canvas) {
-        const unsigned int width = img.columns();
-        const unsigned int height = img.rows();
-        const Magick::PixelPacket* pixels = img.getConstPixels(0, 0, width, height);
-        for(int y = 0; y < height; ++y) {
-                for(int x = 0; x < width; ++x) {
-                        const Magick::PixelPacket& pixel = pixels[y * width + x];
-                        if(pixel.opacity < 255) {
-                                canvas->SetPixel(x, y, pixel.red, pixel.green, pixel.blue);
-                        }
-                }
-        }
-}
-
+void writeImageToCanvas(const Magick::Image &img, rgb_matrix::Canvas* canvas);
 
 class RGBColor : public IToString {
 public:
-       RGBColor(uint8_t r, uint8_t g, uint8_t b) : m_r(r), m_g(g), m_b(b) {}
-       RGBColor(std::string hex) {
-               if(hex.at(0) == '#') {
-                       hex.erase(0, 1);
-               }
-               while(hex.length() != 6) {
-                       hex += "0";
-               }
-               uint8_t r, g, b;
-               sscanf(hex.data(), "%02hhx%02hhx%02hhx", &r, &g, &b);
-               m_r = r;
-               m_g = g;
-               m_b = b;
-       }
-       uint8_t r() const { return m_r; }
-       uint8_t g() const { return m_g; }
-       uint8_t b() const { return m_b; }
+       RGBColor(uint8_t r, uint8_t g, uint8_t b);
+       RGBColor(std::string hex);
+       uint8_t r() const;
+       uint8_t g() const;
+       uint8_t b() const;
 
-       virtual std::string toString() const override {
-               return "RGBColor{r: " + std::to_string(m_r) + ", g: " + std::to_string(m_g) + ", b: " + std::to_string(m_b) + "}";
-       }
+       virtual std::string toString() const override;
 private:
        uint8_t m_r;
        uint8_t m_g;
---
+++ src/utils.cpp
@@ -0,0 +1,37 @@
+#include <utils.h>
+
+void writeImageToCanvas(const Magick::Image &img, rgb_matrix::Canvas* canvas) {
+        const unsigned int width = img.columns();
+        const unsigned int height = img.rows();
+        const Magick::PixelPacket* pixels = img.getConstPixels(0, 0, width, height);
+        for(int y = 0; y < height; ++y) {
+                for(int x = 0; x < width; ++x) {
+                        const Magick::PixelPacket& pixel = pixels[y * width + x];
+                        if(pixel.opacity < 255) {
+                                canvas->SetPixel(x, y, pixel.red, pixel.green, pixel.blue);
+                        }
+                }
+        }
+}
+
+RGBColor::RGBColor(uint8_t r, uint8_t g, uint8_t b) : m_r(r), m_g(g), m_b(b) {}
+RGBColor::RGBColor(std::string hex) {
+    if(hex.at(0) == '#') {
+            hex.erase(0, 1);
+    }
+    while(hex.length() != 6) {
+            hex += "0";
+    }
+    uint8_t r, g, b;
+    sscanf(hex.data(), "%02hhx%02hhx%02hhx", &r, &g, &b);
+    m_r = r;
+    m_g = g;
+    m_b = b;
+}
+uint8_t RGBColor::r() const { return m_r; }
+uint8_t RGBColor::g() const { return m_g; }
+uint8_t RGBColor::b() const { return m_b; }
+
+std::string RGBColor::toString() const {
+    return "RGBColor{r: " + std::to_string(m_r) + ", g: " + std::to_string(m_g) + ", b: " + std::to_string(m_b) + "}";
+}
