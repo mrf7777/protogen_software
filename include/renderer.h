4b3b16d5b6fe6dd3958fdf5d31ed45bd1a790a7f
Michael Fulghum <micryfulg@gmail.com>
2024-10-26 13:20:11 -0500

    split renderer into h and cpp file

--- include/renderer.h
+++ include/renderer.h
@@ -18,27 +18,9 @@ namespace render
 
 class EmotionDrawer final {
 public:
-	EmotionDrawer(const std::string& emotions_directory)
-		: m_emotionsDirectory(emotions_directory)
-	{
-		for(const auto& emotion : ProtogenHeadState::allEmotions()) {
-			// TODO: use std::filesystem to join file system paths.
-			const auto emotion_images_dir = emotions_directory + "/" + emotion;
-			m_emotionImageSpectrums.insert({
-				emotion,
-				image::ImageSpectrum(emotion_images_dir)
-			});
-		}
-	}
-	void draw(rgb_matrix::Canvas& canvas, ProtogenHeadState::Emotion emotion, Proportion eye_openness) const {
-		auto image = m_emotionImageSpectrums.at(emotion).imageForValue(eye_openness);
-		writeImageToCanvas(image, &canvas);
-	}
-	void configWebServerToHostEmotionImages(
-			httplib::Server& srv,
-			const std::string& base_url_path) {
-		srv.set_mount_point(base_url_path, m_emotionsDirectory);
-	}
+	EmotionDrawer(const std::string& emotions_directory);
+	void draw(rgb_matrix::Canvas& canvas, ProtogenHeadState::Emotion emotion, Proportion eye_openness) const;
+	void configWebServerToHostEmotionImages(httplib::Server& srv, const std::string& base_url_path);
 private:
 	std::unordered_map<ProtogenHeadState::Emotion, image::ImageSpectrum> m_emotionImageSpectrums;
 	std::string m_emotionsDirectory;
@@ -46,53 +28,18 @@ private:
 
 class ProtogenHeadFrameProvider final {
 public:
-	ProtogenHeadFrameProvider() {}
-	void draw(rgb_matrix::Canvas& canvas, ProtogenHeadState::Emotion emotion, Proportion mouth_openness, EmotionDrawer& emotion_drawer, image::ImageSpectrum& mouth_images, image::StaticImageDrawer& static_drawer, bool blank, Proportion eye_openness) {
-		if(!blank) {
-			// mouth
-			auto mouth_image = mouth_images.imageForValue(mouth_openness);
-			writeImageToCanvas(mouth_image, &canvas);
-			// emotion
-			emotion_drawer.draw(canvas, emotion, eye_openness);
-			// static
-			static_drawer.drawToCanvas(canvas);
-		}
-	}
+	ProtogenHeadFrameProvider();
+	void draw(rgb_matrix::Canvas& canvas, ProtogenHeadState::Emotion emotion, Proportion mouth_openness, EmotionDrawer& emotion_drawer, image::ImageSpectrum& mouth_images, image::StaticImageDrawer& static_drawer, bool blank, Proportion eye_openness);
 };
-
 
 class MinecraftDrawer final {
 public:
-	MinecraftDrawer()
-	{}
-	void draw(rgb_matrix::Canvas& canvas, const MinecraftState& state) const {
-		drawWorld(canvas, state.blockMatrix(), state.blockColorProfile());
-		drawPlayers(canvas, state);
-	}
+	MinecraftDrawer();
+	void draw(rgb_matrix::Canvas& canvas, const MinecraftState& state) const;
 private:
-	static void drawWorld(rgb_matrix::Canvas& canvas, const mc::BlockMatrix& block_matrix, const mc::BlockColorProfile& color_profile) {
-		for(std::size_t r = 0; r < block_matrix.rows(); r++)
-		{
-			for(std::size_t c = 0; c < block_matrix.cols(); c++)
-			{
-				const auto color = color_profile(block_matrix.get(r, c).value());
-                canvas.SetPixel(c, r, std::get<0>(color), std::get<1>(color), std::get<2>(color));
-			}
-		}
-	}
-	static void drawPlayers(rgb_matrix::Canvas& canvas, const MinecraftState& state) {
-		const auto players = state.players();
-		for(const auto& player_id : players) {
-			state.accessPlayer(player_id, [&canvas, &state](const MinecraftPlayerState& player_state){
-				drawPlayer(canvas, player_state, state.blockColorProfile());
-			});
-		}
-	}
-	static void drawPlayer(rgb_matrix::Canvas& canvas, const MinecraftPlayerState& player_state, const mc::BlockColorProfile& color_profile) {
-		const auto color = color_profile(player_state.selectedBlock());
-		const auto cursor = player_state.cursor();
-        canvas.SetPixel(cursor.second, cursor.first, std::get<0>(color), std::get<1>(color), std::get<2>(color));
-	}
+	static void drawWorld(rgb_matrix::Canvas& canvas, const mc::BlockMatrix& block_matrix, const mc::BlockColorProfile& color_profile);
+	static void drawPlayers(rgb_matrix::Canvas& canvas, const MinecraftState& state);
+	static void drawPlayer(rgb_matrix::Canvas& canvas, const MinecraftPlayerState& player_state, const mc::BlockColorProfile& color_profile);
 };
 
 class IRender {
@@ -101,45 +48,14 @@ public:
     virtual void render(const AppState& data, rgb_matrix::Canvas& canvas) = 0;
 };
 
-class Renderer : public IRender{
+class Renderer : public IRender {
 public:
-    Renderer(EmotionDrawer emotion_drawer, MinecraftDrawer minecraft_drawer, const std::string& mouth_images_dir, const std::string& static_image_path)
-        : m_emotionDrawer(emotion_drawer),
-        m_minecraftDrawer(minecraft_drawer),
-        m_staticImageDrawer(static_image_path),
-        m_headImages(mouth_images_dir),
-        m_protogenHeadFrameProvider()
-    {
-    }
-
-    virtual void render(const AppState& data, rgb_matrix::Canvas& canvas) override {
-        std::lock_guard<std::mutex> lock(m_mutex);
-		switch(data.mode()) {
-		case AppState::Mode::ProtogenHead:
-			viewProtogenHeadData(data.protogenHeadState(), canvas);
-			break;
-		case AppState::Mode::Minecraft:
-			viewMinecraftData(data.minecraftState(), canvas);
-			break;
-		}
-    }
+    Renderer(EmotionDrawer emotion_drawer, MinecraftDrawer minecraft_drawer, const std::string& mouth_images_dir, const std::string& static_image_path);
+    virtual void render(const AppState& data, rgb_matrix::Canvas& canvas) override;
 private:
-	void viewProtogenHeadData(const ProtogenHeadState& data, rgb_matrix::Canvas& canvas) {
-        m_protogenHeadFrameProvider.draw(
-            canvas,
-            data.getEmotionConsideringForceBlink(),
-            data.mouthOpenness(),
-            m_emotionDrawer,
-            m_headImages,
-            m_staticImageDrawer,
-            data.blank(),
-			data.eyeOpenness()
-        );
-	}
+	void viewProtogenHeadData(const ProtogenHeadState& data, rgb_matrix::Canvas& canvas);
 
-	void viewMinecraftData(const MinecraftState& data, rgb_matrix::Canvas& canvas) {
-		m_minecraftDrawer.draw(canvas, data);
-	}
+	void viewMinecraftData(const MinecraftState& data, rgb_matrix::Canvas& canvas);
 
     EmotionDrawer m_emotionDrawer;
     MinecraftDrawer m_minecraftDrawer;
---
+++ src/renderer.cpp
@@ -0,0 +1,115 @@
+#include <renderer.h>
+
+namespace render {
+
+EmotionDrawer::EmotionDrawer(const std::string& emotions_directory)
+    : m_emotionsDirectory(emotions_directory)
+{
+    for(const auto& emotion : ProtogenHeadState::allEmotions()) {
+        // TODO: use std::filesystem to join file system paths.
+        const auto emotion_images_dir = emotions_directory + "/" + emotion;
+        m_emotionImageSpectrums.insert({
+            emotion,
+            image::ImageSpectrum(emotion_images_dir)
+        });
+    }
+}
+
+void EmotionDrawer::draw(rgb_matrix::Canvas& canvas, ProtogenHeadState::Emotion emotion, Proportion eye_openness) const {
+    auto image = m_emotionImageSpectrums.at(emotion).imageForValue(eye_openness);
+    writeImageToCanvas(image, &canvas);
+}
+
+void EmotionDrawer::configWebServerToHostEmotionImages(
+        httplib::Server& srv,
+        const std::string& base_url_path) {
+    srv.set_mount_point(base_url_path, m_emotionsDirectory);
+}
+
+ProtogenHeadFrameProvider::ProtogenHeadFrameProvider() {}
+
+void ProtogenHeadFrameProvider::draw(rgb_matrix::Canvas& canvas, ProtogenHeadState::Emotion emotion, Proportion mouth_openness, EmotionDrawer& emotion_drawer, image::ImageSpectrum& mouth_images, image::StaticImageDrawer& static_drawer, bool blank, Proportion eye_openness) {
+    if(!blank) {
+        // mouth
+        auto mouth_image = mouth_images.imageForValue(mouth_openness);
+        writeImageToCanvas(mouth_image, &canvas);
+        // emotion
+        emotion_drawer.draw(canvas, emotion, eye_openness);
+        // static
+        static_drawer.drawToCanvas(canvas);
+    }
+}
+
+MinecraftDrawer::MinecraftDrawer()
+{}
+
+void MinecraftDrawer::draw(rgb_matrix::Canvas& canvas, const MinecraftState& state) const {
+    drawWorld(canvas, state.blockMatrix(), state.blockColorProfile());
+    drawPlayers(canvas, state);
+}
+
+void MinecraftDrawer::drawWorld(rgb_matrix::Canvas& canvas, const mc::BlockMatrix& block_matrix, const mc::BlockColorProfile& color_profile) {
+    for(std::size_t r = 0; r < block_matrix.rows(); r++)
+    {
+        for(std::size_t c = 0; c < block_matrix.cols(); c++)
+        {
+            const auto color = color_profile(block_matrix.get(r, c).value());
+            canvas.SetPixel(c, r, std::get<0>(color), std::get<1>(color), std::get<2>(color));
+        }
+    }
+}
+
+void MinecraftDrawer::drawPlayers(rgb_matrix::Canvas& canvas, const MinecraftState& state) {
+    const auto players = state.players();
+    for(const auto& player_id : players) {
+        state.accessPlayer(player_id, [&canvas, &state](const MinecraftPlayerState& player_state){
+            drawPlayer(canvas, player_state, state.blockColorProfile());
+        });
+    }
+}
+
+void MinecraftDrawer::drawPlayer(rgb_matrix::Canvas& canvas, const MinecraftPlayerState& player_state, const mc::BlockColorProfile& color_profile) {
+    const auto color = color_profile(player_state.selectedBlock());
+    const auto cursor = player_state.cursor();
+    canvas.SetPixel(cursor.second, cursor.first, std::get<0>(color), std::get<1>(color), std::get<2>(color));
+}
+
+Renderer::Renderer(EmotionDrawer emotion_drawer, MinecraftDrawer minecraft_drawer, const std::string& mouth_images_dir, const std::string& static_image_path)
+    : m_emotionDrawer(emotion_drawer),
+    m_minecraftDrawer(minecraft_drawer),
+    m_staticImageDrawer(static_image_path),
+    m_headImages(mouth_images_dir),
+    m_protogenHeadFrameProvider()
+{
+}
+
+void Renderer::render(const AppState& data, rgb_matrix::Canvas& canvas) {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    switch(data.mode()) {
+    case AppState::Mode::ProtogenHead:
+        viewProtogenHeadData(data.protogenHeadState(), canvas);
+        break;
+    case AppState::Mode::Minecraft:
+        viewMinecraftData(data.minecraftState(), canvas);
+        break;
+    }
+}
+
+void Renderer::viewProtogenHeadData(const ProtogenHeadState& data, rgb_matrix::Canvas& canvas) {
+    m_protogenHeadFrameProvider.draw(
+        canvas,
+        data.getEmotionConsideringForceBlink(),
+        data.mouthOpenness(),
+        m_emotionDrawer,
+        m_headImages,
+        m_staticImageDrawer,
+        data.blank(),
+        data.eyeOpenness()
+    );
+}
+
+void Renderer::viewMinecraftData(const MinecraftState& data, rgb_matrix::Canvas& canvas) {
+    m_minecraftDrawer.draw(canvas, data);
+}
+
+}
