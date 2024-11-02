629a938023701bb6a2aceb1ae820622a25a71bd9
Michael Fulghum <micryfulg@gmail.com>
2024-11-01 21:28:15 -0500

    draft ICanvas interface

---
+++ installable_includes/ICanvas.hpp
@@ -0,0 +1,31 @@
+#include <cstdint>
+
+/**
+ * Represents a surface to draw imagery.
+ */
+class ICanvas {
+public:
+    virtual ~ICanvas() {}
+
+    /**
+     * Width in pixels.
+     */
+    virtual int width() const = 0;
+    /**
+     * Height in pixels.
+     */
+    virtual int height() const = 0;
+    
+    /**
+     * Sets pixel color. 
+     */
+    virtual void setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue) = 0;
+    /**
+     * Sets entire canvas black.
+     */
+    virtual void clear() = 0;
+    /**
+     * Sets entire canvas to provided color.
+     */
+    virtual void fill(uint8_t red, uint8_t green, uint8_t blue) = 0;
+};
