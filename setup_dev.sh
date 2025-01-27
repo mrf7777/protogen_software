869867ed0ad61b5c365928822d1c7d697ccf2d44
Michael Fulghum <micryfulg@gmail.com>
2025-01-26 20:46:18 -0600

    remove sudo from setup_dev.sh

--- setup_dev.sh
+++ setup_dev.sh
@@ -1,28 +1,28 @@
-sudo apt update
+apt update
 
 # required for some utilites in the rpi rgb led matrix library
-sudo apt-get install libgraphicsmagick++-dev libwebp-dev -y
+apt-get install libgraphicsmagick++-dev libwebp-dev -y
 
 # SDL for window-in-desktop visualization and control when
 # real protogen hardware is not avaliable.
-sudo apt-get install libsdl2-dev -y
+apt-get install libsdl2-dev -y
 
 # nginx as a reverse proxy web server
-sudo apt-get install nginx -y
+apt-get install nginx -y
 
 # compile tools
-sudo apt-get install cmake -y
+apt-get install cmake -y
 
 # disable the sound module if not disabled already
 blacklist_file=/etc/modprobe.d/alsa-blacklist.conf
-sudo touch $blacklist_file
-if sudo grep -q "audio_has_been_blacklisted_evidence" $blacklist_file; then
+touch $blacklist_file
+if grep -q "audio_has_been_blacklisted_evidence" $blacklist_file; then
 	echo "Audio already disabled."
 else
-	echo "" | sudo tee -a $blacklist_file
-	echo "# auto-generated audio blacklist from protogen software project setup script" | sudo tee -a $blacklist_file
-	echo "# audio_has_been_blacklisted_evidence" | sudo tee -a $blacklist_file
-	echo "blacklist snd_bcm2835" | sudo tee -a $blacklist_file
+	echo "" | tee -a $blacklist_file
+	echo "# auto-generated audio blacklist from protogen software project setup script" | tee -a $blacklist_file
+	echo "# audio_has_been_blacklisted_evidence" | tee -a $blacklist_file
+	echo "blacklist snd_bcm2835" | tee -a $blacklist_file
 fi
 
 cd ~/rpi-rgb-led-matrix
