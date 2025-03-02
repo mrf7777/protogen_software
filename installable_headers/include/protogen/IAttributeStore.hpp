83ab12983b9fffb61569c1df77f14e292547e633
Michael Fulghum <micryfulg@gmail.com>
2025-03-01 20:32:52 -0600

    simplify standard attribute store and interface

--- installable_headers/include/protogen/IAttributeStore.hpp
+++ installable_headers/include/protogen/IAttributeStore.hpp
@@ -68,37 +68,49 @@ public:
  */
 
 // ID of the object.
-[[maybe_unused]] static const char * A_ID = "id";
+static const char * A_ID = "id";
 // Name of the object.
-[[maybe_unused]] static const char * A_NAME = "name";
+static const char * A_NAME = "name";
 // Description of the object.
-[[maybe_unused]] static const char * A_DESCRIPTION = "description";
+static const char * A_DESCRIPTION = "description";
 // A relative URL path to the thumbnail of the object. This is only the path
 // component and not the full URL. For example, if this attribute has value
 // "thumbnail.png" and the object id is "test", then a full url to this
 // thumbnail from the perspective of this device would be
 // http://0.0.0.0/apps/test/thumbnail.png if your object were an app.
-[[maybe_unused]] static const char * A_THUMBNAIL = "thumbnail";
+static const char * A_THUMBNAIL = "thumbnail";
 // A relative URL path to the main page of the app. See comment on
 // A_THUMBNAIL for details of how this relative URL is used.
 // This is where the user is redirected when an app is launched. This can also
 // be used to locate a control web interface is which controls some device like
 // a sensor or display.
-[[maybe_unused]] static const char * A_MAIN_PAGE = "main_page";
+static const char * A_MAIN_PAGE = "main_page";
 // A URL to the home page of the app. This is where a user can learn more about
 // your object in their free time.
-[[maybe_unused]] static const char * A_HOME_PAGE = "home_page";
+static const char * A_HOME_PAGE = "home_page";
 // A URL which locates where to provide funding for the object.
-[[maybe_unused]] static const char * A_FUNDING = "funding";
+static const char * A_FUNDING = "funding";
 // Version of the object. This should follow Semantic Versioning.
 // See https://semver.org/.
-[[maybe_unused]] static const char * A_VERSION = "version";
+static const char * A_VERSION = "version";
+// This is where your app's resources are located.
+// This attribute is set by the core protogen software when the app is loaded.
+// You can use this to load your resources, or to save data for this app.
+// These files are expected to be read-only. If you need read-write or write 
+// access to files, use the user data directory instead.
+static const char * A_RESOURCES_DIRECTORY = "resources_directory";
+// This is where your app can save user data.
+// This attribute is set by the core protogen software when the app is loaded.
+// This directory is unique to the user and the app. If you need read-write or
+// write access to files, this is the directory to use. If you need read-only
+// access to files, use the resources directory instead.
+static const char * A_USER_DATA_DIRECTORY = "user_data_directory";
 // If this attribute exists, the core protogen software will not use the render
 // method of the app and the core software will not use a thread to do rendering.
 // Instead, the app will receive the render device and the app will be in
 // control of rendering. This is good when apps are not compatible with the
 // threaded rendering system of the core software.
-[[maybe_unused]] static const char * A_CONTROL_RENDER_DEVICE = "control_render_device";
+static const char * A_CONTROL_RENDER_DEVICE = "control_render_device";
 
 } // namespace
 
--- installable_headers/include/protogen/StandardAttributeStore.hpp
+++ installable_headers/include/protogen/StandardAttributeStore.hpp
@@ -6,248 +6,72 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <unordered_set>
+#include <optional>
 #include <utility>
 #include <mutex>
-#include <functional>
+#include <shared_mutex>
 
 namespace protogen {
 
 /**
- * A standard implementation of IAttributeStore that is suitable for most use-cases.
- * 
- * This implementation uses a map to store attributes and their access levels.
- * It is also thread-safe and the underlying data structure is protected by a mutex.
- * 
- * This implementation also provides additional methods that are not part of the IAttributeStore interface,
- * because they are required for the implementation to be managed completely. Rest assured, consumers of this class as an
- * IAttributeStore will not be able to access these methods.
+ * A standard implementation of IAttributeStore that is suitable for most
+ * use-cases. This implementation is thread-safe and can be shared and used
+ * by multiple threads at once.
  * 
  */
 class StandardAttributeStore : public attributes::IAttributeStore {
 public:
-    using ValueAndAccess = std::pair<std::string, Access>;
-    using Attributes = std::unordered_map<std::string, ValueAndAccess>;
-    using PinnedAttributes = std::unordered_set<std::string>;
-
-    using SetAttributePreCallback = std::function<bool(const std::string&, const std::string&)>;
-    using SetAttributePostCallback = std::function<void(const std::string&, const std::string&, SetAttributeResult)>;
-    using RemoveAttributePreCallback = std::function<bool(const std::string&)>;
-    using RemoveAttributePostCallback = std::function<void(const std::string&, RemoveAttributeResult)>;
-
     StandardAttributeStore()
-        : m_attributes(),
-        m_pinnedAttributes(),
-        m_setAttributePreCallback([](const std::string&, const std::string&){ return true; }),
-        m_setAttributePostCallback([](const std::string& , const std::string&, SetAttributeResult){}),
-        m_removeAttributePreCallback([](const std::string&){ return true; }),
-        m_removeAttributePostCallback([](const std::string&, RemoveAttributeResult){})
+        : m_attributes()
     {}
 
-    //
-    // IAttributeStore implementation.
-    //
-
     SetAttributeResult setAttribute(const std::string& key, const std::string& value) override {
-        if(!m_setAttributePreCallback(key, value)) {
-            return SetAttributeResult::UnsetBecauseImplementation;
-        }
-
-        SetAttributeResult result;
-        {
-            std::lock_guard<std::mutex> lock(m_mutex);
-            result = _setAttribute(key, value);
-        }
-        m_setAttributePostCallback(key, value, result);
-        return result;
-    }
-    std::optional<std::string> getAttribute(const std::string& key) const override {
-        std::lock_guard<std::mutex> lock(m_mutex);
+        std::unique_lock lock(m_mutex);
         if(m_attributes.contains(key)) {
-            if(readable(m_attributes.at(key).second)) {
-                return {m_attributes.at(key).first};
-            }
+            m_attributes[key] = value;
+            return SetAttributeResult::Updated;
+        } else {
+            m_attributes[key] = value;
+            return SetAttributeResult::Created;
         }
-        return {};
     }
+
+    std::optional<std::string> getAttribute(const std::string& key) const override {
+        std::shared_lock lock(m_mutex);
+        if(m_attributes.contains(key)) {
+            return m_attributes.at(key);
+        } else {
+            return {};
+        }
+    }
+
     std::vector<std::string> listAttributes() const override {
-        std::lock_guard<std::mutex> lock(m_mutex);
+        std::unique_lock lock(m_mutex);
         std::vector<std::string> attributes;
         for(const auto& [key, _] : m_attributes) {
             attributes.push_back(key);
         }
         return attributes;
     }
-    std::optional<Access> getAttributeAccess(const std::string& key) const override {
-        std::lock_guard<std::mutex> lock(m_mutex);
-        if(m_attributes.contains(key)) {
-            return m_attributes.at(key).second;
-        } else {
-            return {};
-        }
-    }
+
     RemoveAttributeResult removeAttribute(const std::string& key) override {
-        if(!m_removeAttributePreCallback(key)) {
-            return RemoveAttributeResult::KeptBecauseImplementation;
-        }
-
-        RemoveAttributeResult result;
-        {
-            std::lock_guard<std::mutex> lock(m_mutex);
-            result = _removeAttribute(key);
-        }
-        m_removeAttributePostCallback(key, result);
-        return result;
-    }
-    Access getDefaultAccess() const override {
-        return Access::ReadWrite;
-    }
-    bool hasAttribute(const std::string& key) const {
-        return m_attributes.contains(key);
-    }
-
-    //
-    // Implementation details.
-    //
-
-    bool pin(const std::string& key) {
-        std::lock_guard<std::mutex> lock(m_mutex);
+        std::unique_lock lock(m_mutex);
         if(m_attributes.contains(key)) {
-            m_pinnedAttributes.insert(key);
-            return true;
-        }
-        return false;
-    }
-    bool unpin(const std::string& key) {
-        std::lock_guard<std::mutex> lock(m_mutex);
-        if(m_pinnedAttributes.contains(key)) {
-            m_pinnedAttributes.erase(key);
-            return true;
-        }
-        return false;
-    }
-
-    void adminSetAttribute(const std::string& key, const std::string& value, Access access, bool respect_precall = true) {
-        if(!m_setAttributePreCallback(key, value) && respect_precall) {
-            return;
-        }
-        
-        SetAttributeResult result;
-        {
-            std::lock_guard<std::mutex> lock(m_mutex);
-            if(m_attributes.contains(key)) {
-                m_attributes[key] = std::make_pair(value, access);
-                result = SetAttributeResult::Updated;
-            } else {
-                m_attributes[key] = std::make_pair(value, access);
-                result = SetAttributeResult::Created;
-            }
-        }
-        m_setAttributePostCallback(key, value, result);
-    }
-    std::optional<std::string> adminGetAttribute(const std::string& key) const {
-        std::lock_guard<std::mutex> lock(m_mutex);
-        if(m_attributes.contains(key)) {
-            return m_attributes.at(key).first;
-        }
-        return {};
-    }
-    bool adminRemoveAttribute(const std::string& key, bool respect_precall = true) {
-        if(!m_removeAttributePreCallback(key) && respect_precall) {
-            return false;
-        }
-
-        std::size_t elements_erased;
-        {
-            std::lock_guard<std::mutex> lock(m_mutex);
-            elements_erased = m_attributes.erase(key);
-            m_pinnedAttributes.erase(key);
-        }
-        if(elements_erased > 0) {
-            m_removeAttributePostCallback(key, RemoveAttributeResult::Removed);
-            return true;
-        } else {
-            m_removeAttributePostCallback(key, RemoveAttributeResult::DoesNotExist);
-            return false;
-        }
-    }
-
-    void setPreCallbackForSetAttribute(const SetAttributePreCallback& callback) {
-        m_setAttributePreCallback = callback;
-    }
-    void setPostCallbackForSetAttribute(const SetAttributePostCallback& callback) {
-        m_setAttributePostCallback = callback;
-    }
-    void setPreCallbackForSetAttribute(const RemoveAttributePreCallback& callback) {
-        m_removeAttributePreCallback = callback;
-    }
-    void setPostCallbackForRemoveAttribute(const RemoveAttributePostCallback& callback) {
-        m_removeAttributePostCallback = callback;
-    }
-
-private:
-    mutable std::mutex m_mutex;
-    Attributes m_attributes;
-    PinnedAttributes m_pinnedAttributes;
-    SetAttributePreCallback m_setAttributePreCallback;
-    SetAttributePostCallback m_setAttributePostCallback;
-    RemoveAttributePreCallback m_removeAttributePreCallback;
-    RemoveAttributePostCallback m_removeAttributePostCallback;
-
-    static bool readable(Access access) {
-        switch(access) {
-        case Access::Read:
-        case Access::ReadWrite:
-            return true;
-        case Access::Write:
-            return false;
-        default:
-            return false;
-        }
-    }
-
-    static bool writable(Access access) {
-        switch(access) {
-        case Access::Write:
-        case Access::ReadWrite:
-            return true;
-        case Access::Read:
-            return false;
-        default:
-            return false;
-        }
-    }
-
-    SetAttributeResult _setAttribute(const std::string& key, const std::string& value) {
-        if(m_attributes.contains(key)) {
-            if(writable(m_attributes.at(key).second)) {
-                m_attributes.at(key).first = value;
-                return SetAttributeResult::Updated;
-            } else {
-                return SetAttributeResult::UnsetBecauseNotWritable;
-            }
-        } else {
-            m_attributes[key] = std::make_pair(value, getDefaultAccess());
-            return SetAttributeResult::Created;
-        }
-    }
-
-    RemoveAttributeResult _removeAttribute(const std::string& key) {
-        if(m_attributes.contains(key)) {
-            if(writable(m_attributes.at(key).second)) {
-                if(!m_pinnedAttributes.contains(key)) {
-                    m_attributes.erase(key);
-                    return RemoveAttributeResult::Removed;
-                } else {
-                    return RemoveAttributeResult::KeptBecauseImplementation;
-                }
-            } else {
-                return RemoveAttributeResult::KeptBecauseNotWritable;
-            }
+            m_attributes.erase(key);
+            return RemoveAttributeResult::Removed;
         } else {
             return RemoveAttributeResult::DoesNotExist;
         }
     }
+
+    bool hasAttribute(const std::string& key) const {
+        std::shared_lock lock(m_mutex);
+        return m_attributes.contains(key);
+    }
+
+private:
+    mutable std::shared_mutex m_mutex;
+    std::unordered_map<std::string, std::string> m_attributes;
 };
 
 } // namespace
