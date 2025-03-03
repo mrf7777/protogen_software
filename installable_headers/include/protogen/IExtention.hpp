8535a35ad59b6973c1e661ec7ba221a1f78f7262
Michael Fulghum <micryfulg@gmail.com>
2025-03-02 22:16:14 -0600

    document IExtension

--- installable_headers/include/protogen/IExtention.hpp
+++ installable_headers/include/protogen/IExtention.hpp
@@ -7,6 +7,19 @@
 
 namespace protogen {
 
+/**
+ * Represents an extention to the protogen software.
+ * 
+ * An extention is a piece of software that can be dynamically loaded
+ * into the protogen software. An extention can be a sensor, a render
+ * surface, an app, or something else. Extensions are loaded from shared
+ * libraries that are dynamically loaded by the core protogen software.
+ * 
+ * See the documentation for each extention type for more information. Namely:
+ * - ISensor
+ * - IRenderSurface
+ * - IProtogenApp
+ */
 class IExtention {
 public:
     enum class Initialization {
@@ -22,7 +35,8 @@ public:
     virtual Initialization initialize() = 0;
 
     /**
-     * Return attribute store for this extention.
+     * Return attribute store for this extention. This is used to store
+     * metadata, configuration, and other information about the extention.
      */
     virtual std::shared_ptr<attributes::IAttributeStore> getAttributeStore() = 0;
 };
